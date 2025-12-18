d8ee35df71507ec1b8009278f75bb9707de7ddd0
baihongjian <baihongjian2000@163.com>
2025-12-18 19:06:42 +0900

    Implement UserController with get and update methods

--- No_2.ts
+++ No_2.ts
@@ -1,1 +1,22 @@
+export class UserController {
+  constructor(private userService: UserService) {}
 
+  async getUser(req: Request, res: Response) {
+    const userId = req.params.id;
+    const user = await this.userService.findById(userId);
+    
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    
+    return res.json(user);
+  }
+
+  async updateUser(req: Request, res: Response) {
+    const userId = req.params.id;
+    const updateData = req.body;
+    
+    const user = await this.userService.update(userId, updateData);
+    return res.json(user);
+  }
+}
